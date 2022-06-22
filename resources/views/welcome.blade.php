3134ec0499027f2736d3eff2c186670e19f4c366
Karl Irlanda <karlirlanda@gmail.com>
2022-06-22 17:19:00 +0800

    sumukong commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,7 +94,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Deploying Laravel App with RDS to ECS Cluster with CI/CD (Github Actions)
+                                    Deploying Laravel App with RDS to ECS Cluster with CI/CD (Github Actions) - DONE!
                                 </div>
                             </div>
                         </div>
